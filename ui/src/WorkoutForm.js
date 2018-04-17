f7a1857928a478214298268dec91173db262afaa
Marcin Nieborak <marcin.nieborak@gmail.com>
2018-04-17 15:56:59 +0200

    Fix caching and add callback for successful mutation

--- ui/src/WorkoutForm.js
+++ ui/src/WorkoutForm.js
@@ -25,31 +25,40 @@ class WorkoutFormView extends Component {
   handleSubmit(e, Workout) {
     e.preventDefault();
     const { name, sets } = this.state;
-    const parsedSets = sets.map((set) => (
-      {
-        reps: parseInt(set.reps),
-        exercise: (typeof set.exercise === 'string' ? JSON.parse(set.exercise) : set.exercise)
-      }
-    ));
+    const parsedSets = sets.map(set => ({
+      reps: parseInt(set.reps),
+      exercise:
+        typeof set.exercise === "string"
+          ? JSON.parse(set.exercise)
+          : set.exercise
+    }));
 
     Workout({ variables: { name, sets: parsedSets } });
-    //this.setState(this.initialState);
   }
 
-  handleUpdate(cache, { data: { create_workout: workout } }) {
+  handleUpdate(
+    cache,
+    {
+      data: { create_workout: workout }
+    }
+  ) {
     const { workouts } = cache.readQuery({
       query: WorkoutsQuery,
-      variables: { filter: '' }
+      variables: { filter: "" }
     });
     cache.writeQuery({
       query: WorkoutsQuery,
-      data: { workouts: workouts.concat(workout) }
+      data: { workouts: [workout, ...workouts] },
+      variables: { filter: "" }
     });
-
   }
 
+  handleCompleted = () => {
+    this.setState(this.initialState);
+  };
+
   handleInputChange(e, prop) {
-    this.setState({[prop]: e.target.value});
+    this.setState({ [prop]: e.target.value });
   }
 
   handleArrayInputChange(e, i, prop) {
@@ -70,8 +79,8 @@ class WorkoutFormView extends Component {
   }
 
   renderExericseOption(exercise) {
-    return(
-      <option key={`exercise-${exercise.id}`}value={JSON.stringify(exercise)}>
+    return (
+      <option key={`exercise-${exercise.id}`} value={JSON.stringify(exercise)}>
         {exercise.name}
       </option>
     );
@@ -81,28 +90,45 @@ class WorkoutFormView extends Component {
     return (
       <div key={`set-${i}`}>
         <label>Exercise:</label>
-        <select value={set.exercise} onChange={(e) => this.handleArrayInputChange(e, i, 'exercise')}>
-          <option></option>
+        <select
+          value={set.exercise}
+          onChange={e => this.handleArrayInputChange(e, i, "exercise")}
+        >
+          <option />
           {Exercises.map(this.renderExericseOption)}
         </select>
         <label>Reps:</label>
-        <input type='number' min='1' value={set.reps} onChange={(e) => this.handleArrayInputChange(e, i, 'reps')} />
+        <input
+          type="number"
+          min="1"
+          value={set.reps}
+          onChange={e => this.handleArrayInputChange(e, i, "reps")}
+        />
       </div>
     );
   }
 
   render() {
     return (
-      <Mutation mutation={AddWorkoutQuery} update={this.handleUpdate}>
+      <Mutation
+        mutation={AddWorkoutQuery}
+        update={this.handleUpdate}
+        onCompleted={this.handleCompleted}
+      >
         {(Workout, { data }) => (
-          <form onSubmit={(e) => this.handleSubmit(e, Workout)}>
+          <form onSubmit={e => this.handleSubmit(e, Workout)}>
             <div>
               <label>Workout:</label>
-              <input value={this.state.name} onChange={(e) => this.handleInputChange(e, 'name')} />
+              <input
+                value={this.state.name}
+                onChange={e => this.handleInputChange(e, "name")}
+              />
             </div>
             {this.state.sets.map(this.renderExericseLine)}
             <div>
-              <button type="button" onClick={() => this.handleAddSet()}>+ Add next set</button>
+              <button type="button" onClick={() => this.handleAddSet()}>
+                + Add next set
+              </button>
               <button type="submit">Add workout</button>
             </div>
           </form>
